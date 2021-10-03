2d7dec1f4cf9211cd900c093b14f0c1c8079b881
Michael-Petrakis <90578378+Michael-Petrakis@users.noreply.github.com>
2021-10-03 18:04:12 -0500

    Version 1

---
+++ forms/dogStore/dogStore.js
@@ -0,0 +1,16 @@
+/*
+let products = ["Crocheted Dog Toy, $10.00", "Slow Feeder Bowl, $14.00", "Pawz Grey Treat Jar, $20.00", "Blue Sweet Dreams Thermal Pajamas, $30.00", "Checker Chewy Vuiton Bowl, #32.00", "Red Heart Sweater Dress, $27.00", "Velvet Hair Bow - Sun Kissed, $12.00", "Into the Wild Leash, $27.00"]
+let wishList = []
+
+btnWishList.onclick=function(){
+  addWish = inptWishList
+  wishList.push(inptWishList.value)
+  lblWishList.value = wishList
+}
+
+
+btnProducts.onclick=function(){
+  let productList = lblProducts.value
+  lblProducts.value = products
+}
+*/
