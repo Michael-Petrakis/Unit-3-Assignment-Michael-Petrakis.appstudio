2bf96464cf0eeadb4cd84587821af94928c93aab
Michael-Petrakis <90578378+Michael-Petrakis@users.noreply.github.com>
2021-10-03 16:10:53 -0500

    Version 1

---
+++ forms/cityWhile/cityWhile.js
@@ -0,0 +1,18 @@
+/*
+let wantToContinue = true
+let cities = []
+let enterCity = ""
+
+while (wantToContinue) {
+  let enterCity = prompt("Enter a city:")
+  cities.push(enterCity)
+  wantToContinue = confirm("Do you want to enter another city?")
+  }
+  
+  let i = 0
+  
+  while (i < cities.lenght) {
+    console.log(cities[i].toLowerCase())
+    i++
+    }
+*/
